8f0ea94bedd0682e15263a670ad96edc7d5e1684
Tekiter <tekiter8@gmail.com>
2021-11-12 23:51:47 +0900

    Add TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  root: true,
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
     sourceType: "module",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
   plugins: ["react"],
   rules: {
