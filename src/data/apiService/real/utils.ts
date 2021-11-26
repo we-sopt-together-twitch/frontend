3a350a541476866d49e42d5fe3cb0d98c434dd1e
Tekiter <tekiter8@gmail.com>
2021-11-26 22:47:41 +0900

    feat: 추천채널, 인기라이브 Real API 적용

---
+++ src/data/apiService/real/utils.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+export function createAxiosSession(): AxiosInstance {
+  const api = axios.create({
+    baseURL: "https://asia-northeast3-twitchback-5d35f.cloudfunctions.net/api",
+  });
+
+  return api;
+}
