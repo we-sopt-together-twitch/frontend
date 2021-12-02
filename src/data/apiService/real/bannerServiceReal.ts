da3dff1708022db403ee9070db213fb27a71ad55
Suzie Lee <wnsuwls0707@gmail.com>
2021-12-03 00:12:05 +0900

    feat : Chat POST 붙이기

--- src/data/apiService/real/bannerServiceReal.ts
+++ src/data/apiService/real/bannerServiceReal.ts
@@ -11,6 +11,16 @@ export const getBanner = async () => {
     followerCount: item.followerCount,
     messageList: item.messageList,
   }));
+  console.log(`data`, data);
 
+  return result;
+};
+
+export const postBanner = async (message) => {
+  console.log(`message`, message);
+  const { data } = await client.post("/banner/message", message);
+  const serverArr = data.data;
+  const result = serverArr;
+  console.log(`POST data`, data);
   return result;
 };
