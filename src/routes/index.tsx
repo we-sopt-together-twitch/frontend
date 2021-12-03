7145069d818d7292bbe42dddb3bbe959e7e7c692
Tekiter <tekiter8@gmail.com>
2021-12-03 23:02:03 +0900

    chore: 빌드 설정 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { Page404 } from "../pages/404";
 import { PageCategory } from "../pages/Category";
 import { PageHome } from "../pages/Home";
@@ -6,13 +6,21 @@ import { testRoutes } from "./testRoutes";
 
 export function RouteConfig() {
   return (
-    <BrowserRouter>
+    <EnvRouter>
       <Routes>
         <Route path="/" element={<PageHome />} />
         <Route path="/category" element={<PageCategory />} />
         <Route path="/*" element={<Page404 />} />
         {testRoutes}
       </Routes>
-    </BrowserRouter>
+    </EnvRouter>
   );
 }
+
+const EnvRouter = (() => {
+  if (process.env.REACT_APP_ROUTER_MODE === "HASH") {
+    return HashRouter;
+  } else {
+    return BrowserRouter;
+  }
+})();
